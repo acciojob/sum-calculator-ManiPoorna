0642b5c77ae11e2842ac6022ca8f26dfc9b44d6b
Mani Poorna <ndmprao2001@gmail.com>
2023-08-23 19:52:48 +0530

    Version 1.0

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
+
 const App = () => {
+
+  let [sum,setSum] = useState(0);
+
+  const updateSum = (e)=>{
+    let value = parseFloat(e.target.value);
+    setSum((prevState)=> prevState + value);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <input type="number" onChange={(e)=>updateSum(e)}/>
+        <p>Sum : {sum}</p>
     </div>
   )
 }
