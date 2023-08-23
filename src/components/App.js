88bf44173d3348b9095ca9b243a1d2ad9963cdbd
Mani Poorna <ndmprao2001@gmail.com>
2023-08-23 20:05:03 +0530

    Version 1.4

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,7 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
         <input type="number" onChange={(e)=>updateSum(e)}/>
         <p>Sum: {sum}</p>
     </div>
